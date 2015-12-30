5c553245c9868f98426b5056867db786edd37174
arun-kuali <arun.ramaiah@kuali.org>
2015-12-30 15:01:37 +0530

    Initial Commit

---
+++ src/MySelPackage/MySelClass.java
@@ -0,0 +1,35 @@
+package MySelPackage;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.*;
+
+
+import junit.framework.Assert;
+
+public class MySelClass {
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+
+	       //Create a new instance of Firefox Browser
+	       WebDriver driver = new FirefoxDriver();
+
+	       //Open the URL in firefox browser
+	       driver.get("https://www.google.co.in/");
+
+	       //Maximize the Browser window
+	       driver.manage().window().maximize();
+
+	       //Get the current page URL and store the value in variable 'str'
+	       String str = driver.getCurrentUrl();
+
+	       //Print the value of variable in the console
+	       System.out.println("The current URL is " + str);
+	       
+	       Assert.assertEquals("https://www.google.co.in/", str);
+	       
+	       
+	   }
+
+}
